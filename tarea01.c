d105218506e8096f0335797930af927660f52bad
fernandosimbron <fernando.simbron.27@unsch.edu.pe>
2026-09-10 16:42:09 -0500

    implementacion de lab01

--- tarea01.c
+++ tarea01.c
@@ -25,3 +25,24 @@ Proceso* buscarMayorPrioridad(proceso *Procesos, int cantidad) {
 }
 int main() {
    Proceso lista[MAX_PROCESOS]
+//parte B 
+    printf("==REGISTRO DE PROCESOS (% MAX) ==\n", MAX_PROCESOS);
+    for (int i = 0; i < MAX_PROCESOS; i++) {
+    printf("\n--- Proceso %d ---\n", i + 1);
+
+    printf("Ingrese PID: ");
+    scanf("%d", &lista[i],pid);
+
+    printf("Ingrese nombre: ");
+    scanf("%s", lista[i].nombre;
+    
+    do {
+        printf("Ingrese Prioridad (1=alta - 5=baja): ");
+        scanf("%d", &lista[i].prioridad);
+        if (lista[i].prioridad < 1 || lista[i].prioridad > 5) {
+            printf("[!] Prioridad invalida. Debe ser entre 1 y 5.\n");
+        }
+    } while (lista[i].prioridad < 1|| lista[i].prioridad > 5");
+ }
+//llamaba a la funcion 
+Proceso *ganador = buscarMayorPrioridad(lista, MAX_PROCESOS);
